e1cc6b53c9c636f286c2c38bf3ae61888307ea67
herbal-egress <herbal.egress@gmail.com>
2025-12-18 19:46:25 +0300

    Реализация Page Object Model

--- src/test/java/com/saucedemo/base/BasePage.java
+++ src/test/java/com/saucedemo/base/BasePage.java
@@ -1,4 +1,22 @@
 package com.saucedemo.base;
 
-public class BasePage {
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+// Базовый класс для всех Page Object страниц. Обеспечивает инициализацию элементов и базовые ожидания.
+@Slf4j
+public abstract class BasePage {
+    protected final WebDriver driver;
+    protected final WebDriverWait wait;
+
+    protected BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        PageFactory.initElements(driver, this);
+        log.info("Инициализирована страница: {}", this.getClass().getSimpleName());
+    }
 }
